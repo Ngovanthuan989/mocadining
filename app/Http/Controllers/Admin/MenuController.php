9d03ae119928face48c046806fb51f7e2476d1d5
ngovanthuan <ngovanthuan989@gmail.com>
2023-10-24 11:49:29 +0700

    thuanupcode

--- app/Http/Controllers/Admin/MenuController.php
+++ app/Http/Controllers/Admin/MenuController.php
@@ -6,10 +6,20 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Menu;
+use Illuminate\Support\Facades\DB;
+
 
 class MenuController extends Controller
 {
     //
+    public function index()
+    {
+        $get_menu = DB::table('menu')->get();
+
+        return view('admin.menu.show',[
+            'get_menu' => $get_menu
+        ]);
+    }
     public function create()
     {
         return view('admin.menu.create');
@@ -55,7 +65,7 @@ class MenuController extends Controller
 
             if ($validate -> fails()) {
 
-                return redirect()->route('admin.introduce.create')->withErrors($validate);
+                return redirect()->route('admin.menu.create')->withErrors($validate);
 
             }
 
@@ -86,5 +96,215 @@ class MenuController extends Controller
 
         }
         return view('admin.menu.create');
+    }
+
+    public function edit($id)
+    {
+        $edit_menu = DB::table('menu')->where([
+            'id'=> $id
+        ])->get();
+
+        return view('admin.menu.edit',
+            ['edit_menu'=>$edit_menu[0]]
+        );
+    }
+
+    public function update(Request $request)
+    {
+        if($request->file('image') != '' && $request->file('pdfMenu') != '' ){
+            $path = public_path().'/uploads/Menu/images/';
+
+            //upload new file image
+            $file     =    $request -> file('image');
+            $filename =    $file    -> getClientOriginalName();
+            $file -> move($path, $filename);
+
+
+            $pathPdf = public_path().'/uploads/Menu/pdf/';
+
+            //upload new file pdf
+            $filePdf     =    $request -> file('pdfMenu');
+            $filenamePdf =    $filePdf -> getClientOriginalName();
+            $filePdf -> move($pathPdf, $filenamePdf);
+
+
+            $validate = Validator::make(
+
+                $request->all(),
+                [
+                    'title'     => 'required',
+                    'describe'  => 'required',
+
+                ], [
+
+                    'title.required'     => 'Tiêu đề không được bỏ trống',
+                    'describe.required'   => 'Mô tả không được bỏ trống'
+
+                ]
+
+            );
+
+
+
+            if ($validate -> fails()) {
+
+                return redirect()->route('admin.menu.create')->withErrors($validate);
+
+            }
+
+
+            $menu = Menu::where('id', $request->get('id'))->update(array(
+                'title'       => $request->get('title'),
+                'describe'    => $request->get('describe'),
+                'image'       => $filename,
+                'pdfMenu'     => $filenamePdf,
+                'status'      => $request->get('status'),
+            ));
+
+
+            if ($menu == 1) {
+
+                return redirect()->route('admin.menu.edit', ['id' => $request->get('id')])->with('success', 'Cập nhập Menu thành công');
+            } else {
+
+                return redirect()->route('admin.menu.edit', ['id' => $request->get('id')])->with('error', 'Có lỗi xảy ra');
+            }
+        }elseif($request->file('image') != '' && $request->file('pdfMenu') == '' ){
+            $path = public_path().'/uploads/Menu/images/';
+
+            //upload new file image
+            $file     =    $request -> file('image');
+            $filename =    $file    -> getClientOriginalName();
+            $file -> move($path, $filename);
+
+            $validate = Validator::make(
+
+                $request->all(),
+                [
+                    'title'     => 'required',
+                    'describe'  => 'required',
+
+                ], [
+
+                    'title.required'     => 'Tiêu đề không được bỏ trống',
+                    'describe.required'   => 'Mô tả không được bỏ trống'
+
+                ]
+
+            );
+
+
+
+            if ($validate -> fails()) {
+
+                return redirect()->route('admin.menu.create')->withErrors($validate);
+
+            }
+
+
+            $menu = Menu::where('id', $request->get('id'))->update(array(
+                'title'       => $request->get('title'),
+                'describe'    => $request->get('describe'),
+                'image'       => $filename,
+                'status'      => $request->get('status'),
+            ));
+
+
+            if ($menu == 1) {
+
+                return redirect()->route('admin.menu.edit', ['id' => $request->get('id')])->with('success', 'Cập nhập Menu thành công');
+            } else {
+
+                return redirect()->route('admin.menu.edit', ['id' => $request->get('id')])->with('error', 'Có lỗi xảy ra');
+            }
+        }elseif($request->file('pdfMenu') != '' && $request->file('image') == '' ){
+            $pathPdf = public_path().'/uploads/Menu/pdf/';
+
+            //upload new file pdf
+            $filePdf     =    $request -> file('pdfMenu');
+            $filenamePdf =    $filePdf -> getClientOriginalName();
+            $filePdf -> move($pathPdf, $filenamePdf);
+
+            $validate = Validator::make(
+
+                $request->all(),
+                [
+                    'title'     => 'required',
+                    'describe'  => 'required',
+
+                ], [
+
+                    'title.required'     => 'Tiêu đề không được bỏ trống',
+                    'describe.required'   => 'Mô tả không được bỏ trống'
+
+                ]
+
+            );
+
+
+
+            if ($validate -> fails()) {
+
+                return redirect()->route('admin.menu.create')->withErrors($validate);
+
+            }
+
+
+            $menu = Menu::where('id', $request->get('id'))->update(array(
+                'title'       => $request->get('title'),
+                'describe'    => $request->get('describe'),
+                'status'      => $request->get('status'),
+                'pdfMenu'     => $filenamePdf,
+            ));
+
+
+            if ($menu == 1) {
+
+                return redirect()->route('admin.menu.edit', ['id' => $request->get('id')])->with('success', 'Cập nhập Menu thành công');
+            } else {
+
+                return redirect()->route('admin.menu.edit', ['id' => $request->get('id')])->with('error', 'Có lỗi xảy ra');
+            }
+        }else {
+
+            $validate = Validator::make(
+
+                $request->all(),
+                [
+                    'title'      => 'required',
+                    'describe'   => 'required',
+
+                ], [
+
+                    'title.required'     => 'Tiêu đề không được bỏ trống',
+                    'describe.required'   => 'Mô tả không được bỏ trống'
+
+                ]
+
+            );
+
+
+            if ($validate -> fails()) {
+
+                return redirect()->route('admin.menu.create')->withErrors($validate);
+
+            }
+
+
+            $menu = Menu::where('id', $request->get('id'))->update(array(
+                'title'       => $request->get('title'),
+                'describe'    => $request->get('describe'),
+                'status'      => $request->get('status'),
+            ));
+
+
+            if ($menu == 1) {
+
+                return redirect()->route('admin.menu.edit', ['id' => $request->get('id')])->with('success', 'Cập nhập menu thành công');
+            } else {
+
+                return redirect()->route('admin.menu.edit', ['id' => $request->get('id')])->with('error', 'Có lỗi xảy ra');
+            }
+        }
     }
 }
---
+++ resources/views/admin/menu/show.blade.php
@@ -0,0 +1,87 @@
+@extends('admin.layout.master')
+
+@section('main')
+    <!-- partial -->
+    <div class="main-panel">
+        <div class="content-wrapper">
+            <div class="row">
+                <div class="col-lg-12 grid-margin stretch-card">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title">Danh sách thực đơn</h4>
+                            <a href="/admin/menu/create" class="btn btn-success p-2 px-5 mb-3">Thêm
+                                mới</a>
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th>STT</th>
+                                            <th>Tiêu đề</th>
+                                            <th>Mô tả</th>
+                                            <th>Chi tiết thực đơn</th>
+                                            <th>Ngày tạo</th>
+                                            <th>Trạng thái</th>
+                                            <th>Tác vụ</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody id="content-body">
+                                        @foreach ($get_menu as $get_menus)
+                                            <tr>
+                                                <td>{{$get_menus->id}}</td>
+                                                <td>{{$get_menus->title}}</td>
+                                                <td>{{$get_menus->describe}}</td>
+
+                                                <td class="d-flex" style="gap: 10px">
+                                                    <a href="/uploads/Menu/pdf/{{$get_menus->pdfMenu}}"
+                                                        class="btn-success px-3"
+                                                        style="border-radius:5px;display:flex;align-items: center;">Xem
+                                                    </a>
+                                                </td>
+                                                <td>{{$get_menus->created_at}}</td>
+                                                <td>
+                                                    @if ($get_menus->status == 1) Hoạt động @else tắt @endif
+                                                </td>
+                                                <td class="d-flex" style="gap: 10px">
+                                                    <a href="/admin/menu/edit/{{$get_menus->id}}"
+                                                        class="btn-success px-3"
+                                                        style="border-radius:5px;display:flex;align-items: center;">Sửa</a>
+                                                    {{-- <a href="/admin/promotion/47/edit?locale=th"
+                                                        class="btn-success px-3"
+                                                        style="border-radius:5px;display:flex;align-items: center;">th</a>
+                                                    <a href="/admin/promotion/47/edit?locale=vi"
+                                                        class="btn-success px-3"
+                                                        style="border-radius:5px;display:flex;align-items: center;">vi</a> --}}
+                                                    <form action="/admin/menu/delete" method="POST">
+                                                        @csrf
+                                                         <input
+                                                            name="id" type="hidden" value="{{$get_menus->id}}">
+                                                        <button type="submit" class="btn btn-danger">Xóa</button>
+                                                    </form>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- content-wrapper ends -->
+        <!-- partial:partials/_footer.html -->
+        <footer class="footer">
+            <div class="d-sm-flex justify-content-center justify-content-sm-between"
+                style="justify-content: end !important;">
+                <span style="    color: gold !important;"
+                    class="text-muted d-block text-center text-sm-left d-sm-inline-block"></span>
+            </div>
+        </footer>
+        <!-- partial -->
+    </div>
+    <!-- main-panel ends -->
+@endsection
+
+@section('js')
+@endsection
